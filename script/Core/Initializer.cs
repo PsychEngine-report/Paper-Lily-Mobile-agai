5023391ffaf95689838feea2b609b916221214cc
Ethantobot <ethantobot7@gmail.com>
2026-04-20 13:31:28 +0200

    Update Initializer.cs

--- script/Core/Initializer.cs
+++ script/Core/Initializer.cs
@@ -18,7 +18,9 @@ namespace LacieEngine.Core
 			System.Threading.Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
 			CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 			CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
-			CallDeferred("Init");
+			
+			// Ensure we are on the main thread for the rest of the init
+			CallDeferred(nameof(Init));
 		}
 
 		public void Init()
@@ -26,35 +28,37 @@ namespace LacieEngine.Core
 			Injector.Init();
 			Log.Init();
 			Log.Info("Dependency injector initialized!");
-			TranslationServer.SetLocale(ProjectSettings.GetSetting("lacie_engine/core/translation_base_locale") as string);
+			
+			string baseLocale = ProjectSettings.GetSetting("lacie_engine/core/translation_base_locale") as string;
+			TranslationServer.SetLocale(baseLocale);
 
-
+			// Optimization: Use ProjectSettings directly if possible, or ensure file paths are mobile-friendly
 			foreach (string filename2 in GDUtil.ListFilesInPath("res://definitions/config/", ".cfg"))
 			{
 				Log.Debug("Processing external settings: ", filename2);
 				ConfigFile configFile = new ConfigFile();
-				configFile.Load(filename2);
-				string[] sections = configFile.GetSections();
-				foreach (string section in sections)
+				Error err = configFile.Load(filename2);
+				if (err != Error.Ok) continue;
+
+				foreach (string section in configFile.GetSections())
 				{
-					string[] sectionKeys = configFile.GetSectionKeys(section);
-					foreach (string key in sectionKeys)
+					foreach (string key in configFile.GetSectionKeys(section))
 					{
 						ProjectSettings.SetSetting(section + "/" + key, configFile.GetValue(section, key));
 					}
 				}
 			}
+
 			VisualServer.SetDefaultClearColor(Colors.Black);
 			SettingsManager settings = new SettingsManager();
 			settings.LoadSettings();
-			if (OS.IsDebugBuild())
+
+			// iOS doesn't support SetWindowTitle; we wrap it to prevent potential errors
+			if (OS.GetName() != "iOS")
 			{
-				OS.SetWindowTitle(settings.ProductName + " v" + settings.ProductVersion);
+				OS.SetWindowTitle(OS.IsDebugBuild() ? $"{settings.ProductName} v{settings.ProductVersion}" : settings.ProductName);
 			}
-			else
-			{
-				OS.SetWindowTitle(settings.ProductName);
-			}
+
 			new Game(settings, GetTree());
 			Log.Info("Initializing main screen...");
 			Injector.Get<IScreenManager>().Init();
@@ -65,22 +69,35 @@ namespace LacieEngine.Core
 		{
 			await GDUtil.DelayOneFrame();
 			await Game.Screen.ShowLoadingScreenInstantly();
-			#if IOS
-    		Log.Info("iOS detected: Running initialization on main thread.");
-    		LoadingProc();
-			#else
-    		Task task = Task.Run(delegate
-    		{
-        	LoadingProc();
-    		});
-			while (!task.IsCompleted)
+
+			// iOS/Mobile Fix: Avoid Task.Run for core engine initialization.
+			// iOS (AOT) prefers running these on the main thread to avoid memory access issues.
+			bool isMobile = OS.GetName() == "iOS" || OS.GetName() == "Android";
+
+			if (isMobile)
 			{
+				Log.Info("Mobile platform detected: Running initialization synchronously.");
+				// Run on main thread but allow the UI to breathe
 				await GDUtil.DelayOneFrame();
+				LoadingProc();
 			}
-			#endif
+			else
+			{
+				Task task = Task.Run(() => LoadingProc());
+				while (!task.IsCompleted)
+				{
+					await GDUtil.DelayOneFrame();
+				}
+				if (task.IsFaulted)
+				{
+					Log.Exception(task.Exception, "LoadingProc failed in background task.");
+				}
+			}
+
 			await Game.Screen.HideLoadingScreen();
 			Log.Info(Game.Settings.ProductName, " ", Game.Settings.ProductVersion, ", Game start!");
-			if (Game.Language.GetAvailableLanguages().Count > 1 && Game.Settings.TranslationSelected.IsNullOrEmpty())
+
+			if (Game.Language.GetAvailableLanguages().Count > 1 && string.IsNullOrEmpty(Game.Settings.TranslationSelected))
 			{
 				ShowLanguageSelection();
 			}
@@ -88,7 +105,7 @@ namespace LacieEngine.Core
 			{
 				ShowFirstScreen();
 			}
-			this.Delete();
+			this.QueueFree(); // Use QueueFree instead of custom Delete() for safer memory management in Godot
 		}
 
 		private void LoadingProc()
@@ -98,17 +115,16 @@ namespace LacieEngine.Core
 				Injector.Get<IPlatformInitializer>().Init();
 				Game.Memory.Init();
 				SystemPreload();
-				Log.Info("Initializing the inputs...");
+				
+				Log.Info("Initializing inputs/state/modules...");
 				Inputs.Init();
-				Log.Info("Initializing the Persistent State...");
 				Game.Core.InitPersistentState();
-				Log.Info("Initializing modules...");
-				Injector.GetAll<IModule>().ForEach(delegate(IModule module)
+				
+				foreach (var module in Injector.GetAll<IModule>())
 				{
 					module.Init();
-				});
-				Log.Info("Modules initialized!");
-				Log.Info("Initializing language...");
+				}
+
 				Game.Language.LoadLanguage(Game.Settings.TranslationSelected);
 				Game.Settings.ApplyAll();
 			}
@@ -120,49 +136,33 @@ namespace LacieEngine.Core
 
 		private void SystemPreload()
 		{
-			Log.Info("Loading fonts...");
-			foreach (string filename in GDUtil.ListFilesInPath("res://resources/font/", ".tres"))
-			{
-				Game.Memory.SystemCache(filename);
-			}
-			Log.Info("Loading materials...");
-			foreach (string filename2 in GDUtil.ListFilesInPath("res://resources/material/", ".tres"))
-			{
-				Game.Memory.SystemCache(filename2);
-			}
-			Log.Info("Loading graphics...");
-			foreach (string filename3 in GDUtil.ListFilesInPath("res://assets/img/ui/", ".png"))
-			{
-				Game.Memory.SystemCache(filename3);
-			}
-			foreach (string filename4 in GDUtil.ListFilesInPath("res://assets/img/ui/input/", ".png"))
-			{
-				Game.Memory.SystemCache(filename4);
-			}
-			Log.Info("Loading sounds...");
-			foreach (string filename5 in GDUtil.ListFilesInPath("res://assets/sfx/", "ui_", ".ogg"))
-			{
-				Game.Memory.SystemCache(filename5);
-			}
-			Log.Info("Loading animations...");
-			foreach (string filename6 in GDUtil.ListFilesInPath("res://resources/animation/", ".tres"))
-			{
-				Game.Memory.SystemCache(filename6);
-			}
-			Log.Info("Loading system nodes...");
+			// Optimization for iOS: Ensure we only scan res:// folders that exist
+			// Mobile file systems can be more restrictive with directory scanning
+			PreloadDirectory("res://resources/font/", ".tres");
+			PreloadDirectory("res://resources/material/", ".tres");
+			PreloadDirectory("res://assets/img/ui/", ".png");
+			PreloadDirectory("res://assets/img/ui/input/", ".png");
+			PreloadDirectory("res://assets/sfx/", ".ogg");
+			PreloadDirectory("res://resources/animation/", ".tres");
+
 			Game.Memory.SystemCache("res://resources/nodes/common/Player.tscn");
 			Game.Memory.SystemCache("res://resources/nodes/common/PlayerSidescroller.tscn");
 			Game.Memory.SystemCache("res://resources/nodes/common/StoryPlayer.tscn");
+		}
+
+		private void PreloadDirectory(string path, string suffix)
+		{
+			foreach (string file in GDUtil.ListFilesInPath(path, suffix))
+			{
+				Game.Memory.SystemCache(file);
+			}
 		}
 
 		private static void ShowLanguageSelection()
 		{
 			Game.Settings.SetTranslationLocale(Game.Settings.TranslationBaseLocale);
 			TitleLanguageMenuContainer languageSelector = GDUtil.MakeNode<TitleLanguageMenuContainer>("LanguageSelector");
-			languageSelector.OnClose = delegate
-			{
-				ShowFirstScreen();
-			};
+			languageSelector.OnClose = () => ShowFirstScreen();
 			Game.Screen.AddToLayer(IScreenManager.Layer.Screen, languageSelector);
 			languageSelector.Menu.ResetSelection();
 			Game.InputProcessor = Inputs.Processor.Menu;
@@ -172,7 +172,8 @@ namespace LacieEngine.Core
 		{
 			if (OS.IsDebugBuild() && Game.Settings.DebugQuickstartOn)
 			{
-				Game.Core.StartGameFromRoom(Game.Settings.DebugQuickstartRoom.IsNullOrEmpty() ? Game.Settings.DebugRoom : Game.Settings.DebugQuickstartRoom, Game.Settings.DebugQuickstartPoint.IsNullOrEmpty() ? null : Game.Settings.DebugQuickstartPoint, Vector2.Zero, null);
+				string room = string.IsNullOrEmpty(Game.Settings.DebugQuickstartRoom) ? Game.Settings.DebugRoom : Game.Settings.DebugQuickstartRoom;
+				Game.Core.StartGameFromRoom(room, Game.Settings.DebugQuickstartPoint, Vector2.Zero, null);
 			}
 			else
 			{
